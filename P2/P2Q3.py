d33e3b80f41a458db513c58c5b10fdf882a1191b
Ori Cohen <79476840+OriCohen05@users.noreply.github.com>
2024-05-06 02:33:00 +0300

    Update Q3 With better naming conventions

--- P2/P2Q3.py
+++ P2/P2Q3.py
@@ -1,13 +1,9 @@
-def is_sorted_polyndrom(str: str) -> bool:
+def is_sorted_polyndrom(text: str) -> bool:
         is_sorted = True
-        is_poly = str[::-1] == str
-        iteration = len(str) // 2 if len(str) % 2 == 0 else len(str) // 2 + 1
+        is_poly = str[::-1] == text
+        iteration = len(text) // 2
         for i in range(1, iteration + 1):
-            if ord(str[i]) < ord(str[i - 1]):
-                is_sorted = False
-                return is_sorted
+            if ord(text[i]) < ord(text[i - 1]):
+                return False
 
         return is_sorted and is_poly
-
-
-print(is_sorted_polyndrom("cbabc"))
